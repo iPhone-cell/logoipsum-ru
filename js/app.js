726767cf7a1dbdbb68d2b44162585036af2dfe67
iPhone-cell <53273668+iPhone-cell@users.noreply.github.com>
2022-10-31 20:04:15 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -16,15 +16,23 @@ $(function () {
 	// Nav Toggle
 	let nav = $("#nav");
 	let navToggle = $("#navToggle");
+	let burgerItem = $('#burgerItem');
 	navToggle.on("click", function (event) {
 		event.preventDefault();
 
 		nav.toggleClass("show");
+		burgerItem.toggleClass("active");
 	});
 
+	// Tabs
+	$('.tabs-wrapper').each(function () {
+		let ths = $(this);
+		ths.find('.tab-item').not(':first').hide();
+		ths.find('.tab').click(function () {
+			ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
+			ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
+		}).eq(0).addClass('active');
+	});
 });
 
-// Tabs
-$(function () {
-	$("#tabs").tabs();
-});
+
